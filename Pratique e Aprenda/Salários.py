bfd6c11e974b3f997ba4cce97a0acd0fbcb25fdd
ANDRE MOREIRA DE SOUZA <andrems1991@gmail.com>
2024-09-13 16:27:48 -0300

    Add files via upload

---
+++ Pratique e Aprenda/Salários.py
@@ -0,0 +1,8 @@
+saudacao = print("Olá, seus dados salariais são:")
+
+nome = input("nome do funcionario:")
+
+salário_mensal = float(input("seu salário mensal é:"))
+horas_trabalhadas_por_semana = float(input("suas horas trabalhadas por semana é:"))
+print(horas_trabalhadas_por_semana * 4)
+print(salário_mensal / 160)
---
+++ Pratique e Aprenda/Usuario.py
@@ -0,0 +1,9 @@
+saudação = print("Seus dados de usuário, são:")
+
+nome_usuário = input("Por favor, insira seu nome de usuário:")
+idade_usuário = input("Por favor, insira sua idade:")
+cidade_usuário = input("Por favor, digite sua cidade natal:")
+
+print(f"O nome do usuário é:{nome_usuário}")
+print(f"A idade do usuário é:{idade_usuário}")
+print(f"A cidade natal do usuário é:{cidade_usuário}")
